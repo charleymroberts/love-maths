d089837075c83bd0e38ccad4730ea6cd4281377d
Charley Roberts <charley.m.roberts@gmail.com>
2022-12-08 19:06:50 +0000

    Displays addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * Generates two random numbers between 1 and 25 to be used in the calculation the user is asked to make
  */
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`)
+        throw(`Unknown game type: ${gameType}. Aborting!`)
+    }
 }
 
 function checkAnswer() {
@@ -38,8 +47,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
